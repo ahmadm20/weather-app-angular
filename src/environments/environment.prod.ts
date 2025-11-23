087f72eb698faa7fc6fa9a41f4eef7b2b3810f61
ahmadm20 <113246524+ahmadm20@users.noreply.github.com>
2025-11-23 12:50:33 +0100

    Add build output and updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  weatherApiBaseUrl: 'https://DEINE-API-URL-HIER',
+
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'DEIN-RAPIDAPI-HOST',
+
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'DEIN-RAPIDAPI-KEY',
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: false,
-   weatherApiBaseUrl:'https://openweather43.p.rapidapi.com/weather',
-  
 
-  XRapidAPIHostHeaderName :'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue:'openweather43.p.rapidapi.com',
-  XRapidAPIKeyHeaderName :'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue :'a467d8d03cmsh2e1a5d6f924078dp19097fjsn77b48016c6f1',
+  weatherApiBaseUrl: 'https://DEINE-API-URL-HIER',
+
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'DEIN-RAPIDAPI-HOST',
+
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'DEIN-RAPIDAPI-KEY',
 };
